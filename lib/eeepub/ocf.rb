e509482b22182a94e6a26da63530a4b281be433c
Drew Bowman <drew@12spokes.com>
2012-09-26 10:38:40 -0400

    removed logging

--- lib/eeepub/ocf.rb
+++ lib/eeepub/ocf.rb
@@ -98,9 +98,6 @@ module EeePub
         end
         zipfile = Zip::ZipFile.open(output_path)
         Dir.glob('**/*').each do |path|
-          puts path
-          puts "READABLE: #{File.readable?(path)}"
-          puts "SIZE: #{File.size(path)}"
           zipfile.add(path, path)
         end
         zipfile.commit
